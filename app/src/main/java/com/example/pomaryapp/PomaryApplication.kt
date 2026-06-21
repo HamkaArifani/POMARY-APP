e1e1d6b214f4c0a0ba0551b0a9a87dc5e6a01c39
HamkaArifani <hamka0897@gmail.com>
2026-06-21 22:43:48 +0800

    feat: implement PomaryApplication

---
+++ app/src/main/java/com/example/pomaryapp/PomaryApplication.kt
@@ -0,0 +1,16 @@
+package com.example.pomaryapp
+
+import android.app.Application
+import android.content.pm.ApplicationInfo
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class PomaryApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if ((applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE)!= 0){
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
