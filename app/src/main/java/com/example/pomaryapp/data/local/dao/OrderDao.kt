8a6b5e36d330b45411d3fba6cd2c903fbcea8164
HamkaArifani <hamka0897@gmail.com>
2026-06-23 00:33:40 +0800

    Implement dao

--- app/src/main/java/com/example/pomaryapp/data/local/dao/OrderDao.kt
+++ app/src/main/java/com/example/pomaryapp/data/local/dao/OrderDao.kt
@@ -1,4 +1,28 @@
 package com.example.pomaryapp.data.local.dao
 
-class OrderDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.pomaryapp.data.local.entity.OrderEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface OrderDao {
+    @Query("SELECT * FROM orders WHERE preorderId = :preorderId")
+    fun getOrdersByPreorder(preorderId: String): Flow<List<OrderEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertOrder(order: OrderEntity)
+
+    @Update
+    suspend fun updateOrder(order: OrderEntity)
+
+    @Delete
+    suspend fun deleteOrder(order: OrderEntity)
+
+    @Query("SELECT * FROM orders WHERE orderId = :id")
+    suspend fun getOrderById(id: String): OrderEntity?
 }
--- app/src/main/java/com/example/pomaryapp/data/local/dao/PreorderDao.kt
+++ app/src/main/java/com/example/pomaryapp/data/local/dao/PreorderDao.kt
@@ -1,4 +1,28 @@
 package com.example.pomaryapp.data.local.dao
 
-class PreorderDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.pomaryapp.data.local.entity.PreorderEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PreorderDao {
+    @Query("SELECT * FROM preorders ORDER BY end_date DESC")
+    fun getAllPreorders(): Flow<List<PreorderEntity>>
+
+    @Query("SELECT * FROM preorders WHERE preorderId= :preorderId")
+    suspend fun getPreorderById(preorderId: String): PreorderEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertPreorder(preorder: PreorderEntity)
+
+    @Update
+    suspend fun updatePreorder(preorder: PreorderEntity)
+
+    @Delete
+    suspend fun deletePreorder(preorder: PreorderEntity)
 }
