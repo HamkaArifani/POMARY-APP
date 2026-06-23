9aa2866bea9e6840647554ea11ed633dacf264d8
HamkaArifani <hamka0897@gmail.com>
2026-06-23 09:29:04 +0800

    implement PomaryDatabase

--- app/src/main/java/com/example/pomaryapp/data/local/PomaryDatabase.kt
+++ app/src/main/java/com/example/pomaryapp/data/local/PomaryDatabase.kt
@@ -1,4 +1,19 @@
 package com.example.pomaryapp.data.local
 
-class PomaryDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.pomaryapp.data.local.dao.OrderDao
+import com.example.pomaryapp.data.local.dao.PreorderDao
+import com.example.pomaryapp.data.local.entity.OrderEntity
+import com.example.pomaryapp.data.local.entity.PreorderEntity
+import com.example.pomaryapp.data.local.entity.UserEntity
+
+@Database(
+    entities = [UserEntity::class, PreorderEntity::class, OrderEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class PomaryDatabase: RoomDatabase() {
+    abstract fun preorderDao(): PreorderDao
+    abstract fun orderDao(): OrderDao
 }
