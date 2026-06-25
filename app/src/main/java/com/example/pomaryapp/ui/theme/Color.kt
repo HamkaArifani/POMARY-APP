2b0758599c0983acb7d1f63203b97229a83d5af4
HamkaArifani <hamka0897@gmail.com>
2026-06-25 21:42:31 +0800

    feat: init color for screen

--- app/src/main/java/com/example/pomaryapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/pomaryapp/ui/theme/Color.kt
@@ -11,4 +11,4 @@ val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 val PinScreenBackground = Color(0xFF2A4DF1)
 val PreorderCard = Color(0xFF2287E0)
-
+val PinScreenBackgrounds = Color(0xFFF8F6F6)
