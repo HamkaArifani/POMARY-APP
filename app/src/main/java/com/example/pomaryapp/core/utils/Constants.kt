6040683e94f1d82996528c8481e554aed8d7b3f9
HamkaArifani <hamka0897@gmail.com>
2026-06-29 20:32:30 +0800

    delete unused constants

--- app/src/main/java/com/example/pomaryapp/core/utils/Constants.kt
+++ app/src/main/java/com/example/pomaryapp/core/utils/Constants.kt
@@ -2,14 +2,7 @@ package com.example.pomaryapp.core.utils
 
 object Constants {
     const val DATABASE_NAME = "pomary_db"
-
     const val DATASTORE_NAME = "pomary_prefs"
-    const val KEY_PIN = "user_pin"
-    const val KEY_IS_LOGGED_IN = "is_logged_in"
-    const val KEY_USER_NAME = "user_name"
-
-    const val MAX_PIN_ATTEMPTS = 5
-    const val LOCKOUT_DURATION_MS = 30 * 60 * 1000L
 
     const val COLL_USERS = "users"
     const val COLL_PREORDERS = "preorders"
